fab8c0a25422d2ce6cd076c0e17a75c7e8ae67a1
rudskaya irina <irarudskaya5@gmail.com>
2023-04-06 15:00:13 +0300

    Don't touch master branch without check:)

--- src/skypro/java/course2/theory/Main.java
+++ src/skypro/java/course2/theory/Main.java
@@ -1,21 +1,7 @@
 package skypro.java.course2.theory;
 
-//- Задача на лайв-кодинг
-//        Напишите метод, который возводит число в куб.
+public class Main {
+    public static void main(String[] args) {
 
-import java.util.List;
-import java.util.Arrays;
-
-public class Main{
-    public static void main(String[] args){
-        List<Integer> numbers = Arrays.asList(1, 2, 3);
-        cube(numbers);
-    }
-
-    public static<C extends Number> void cube(List<C> list){
-        list.stream()
-                .mapToDouble(Number::doubleValue)
-                .map(number -> Math.pow(number, 3))
-                .forEach(System.out::println);
     }
 }
